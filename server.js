3cbcc0dd946214325e885b7077ffb0cdfa58430b
He Yuan <yuan.he@orange.com>
2018-04-25 11:12:36 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require('express'),
   bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/ApiAxiomeDB');
+mongoose.connect('mongodb://mongo/ApiAxiomeDB');
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
